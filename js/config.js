43ebeadef3ee906e783b90cdd8d11247f6e07987
LweoIsHere <75079581+LweoIsHere@users.noreply.github.com>
2020-11-26 13:17:56 -0600

    uwu

--- js/config.js
+++ js/config.js
@@ -231,10 +231,6 @@ var l_musicPlaylist = [
 	{ogg: "lullaby.ogg", name: "Lullaby of Deserted Hell"},
 
 	{ogg: "falls_in_fall.ogg", name: "秋に舞う椛と滝飛沫"},
-
-	{youtube: "https://www.youtube.com/watch?v=3VTwbEWW2W8", name: "Tea for Two"},
-
-	{youtube: "https://www.youtube.com/watch?v=VaDN_19rPyw", name: "Voile, the Magic Library"},
 	
 	{youtube: "https://youtu.be/WS6Ien9YUiI", name: "【東方ボーカル】 「ニヒル神楽」 【幽閉サテライト】【Subbed】"},
 	
